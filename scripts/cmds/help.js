8faea1bee39c9c5e25572cf8c3938190c3e7e807
shakib99017 <mr7754720@gmail.com>
2026-09-08 01:34:52 +0600

    Update admin contact information in help command

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -63,7 +63,7 @@ module.exports = {
                                        `Type ${prefix}help <cmd> to see details.`;
 
                         msg += `\n\n⭔ Total Commands: ${totalCommands}\n⭔ ${helpHint}\n`;
-                        msg += `\n╭─✦ ADMIN: MahMUD 彡\n├‣ WHATSAPP\n╰‣ 01836298139`;
+                        msg += `\n╭─✦ ADMIN: Shakib bhaiya 彡\n├‣ WHATSAPP\n╰‣ 01322418218`;
 
                         try {
                                 const hh = await message.reply({ body: msg });
